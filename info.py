ed04a7baea9d4277d7bd2761fe095e3f3f9a27e0
WayK17 <109327502+WayK17@users.noreply.github.com>
2025-03-04 15:01:18 -0500

    Actualizar info.py

--- info.py
+++ info.py
@@ -33,11 +33,11 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'My_Tg_files')
 #----------- There will be channel id add in all these ---------
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '0'))  # set shortner log channel
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','-1002316599591')) # The movie you upload in it will be deleted from the bot.
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
-auth_channel = environ.get('AUTH_CHANNEL', '')
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
-request_channel = environ.get('REQUEST_CHANNEL', '0') # If anyone sends a request message to your bot, you will get it in this channel.
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0')) # 
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002260222146'))
+auth_channel = environ.get('AUTH_CHANNEL', '-1002260222146')
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002409857165'))
+request_channel = environ.get('REQUEST_CHANNEL', '-1002409857165') # If anyone sends a request message to your bot, you will get it in this channel.
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002449546925')) # 
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/NESS_Soporte') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
